c638964026cd2fe5ea3513e25d117ab0a9a050eb
Diego Elias <eliaslopezdiegojosue@gmail.com>
2025-02-27 20:34:49 -0600

    subiendo el pryecto inicial

---
+++ SistemaDeGestionDeClimas/Graficas.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SistemaDeGestionDeClimas
+{
+    partial class Graficas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            grafica1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label1 = new Label();
+            btnGraficar = new Button();
+            cbRegionGraficar = new ComboBox();
+            btnRegresar = new Button();
+            ((System.ComponentModel.ISupportInitialize)grafica1).BeginInit();
+            SuspendLayout();
+            // 
+            // grafica1
+            // 
+            grafica1.BackColor = Color.Transparent;
+            grafica1.BorderlineWidth = 4;
+            grafica1.BorderSkin.BorderWidth = 2;
+            chartArea1.Name = "ChartArea1";
+            grafica1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            grafica1.Legends.Add(legend1);
+            grafica1.Location = new Point(16, 90);
+            grafica1.Name = "grafica1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            grafica1.Series.Add(series1);
+            grafica1.Size = new Size(705, 348);
+            grafica1.TabIndex = 0;
+            grafica1.Text = "chart1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(117, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // btnGraficar
+            // 
+            btnGraficar.Location = new Point(231, 60);
+            btnGraficar.Name = "btnGraficar";
+            btnGraficar.Size = new Size(121, 23);
+            btnGraficar.TabIndex = 3;
+            btnGraficar.Text = "button1";
+            btnGraficar.UseVisualStyleBackColor = true;
+            btnGraficar.Click += btnGraficar_Click;
+            // 
+            // cbRegionGraficar
+            // 
+            cbRegionGraficar.FormattingEnabled = true;
+            cbRegionGraficar.Items.AddRange(new object[] { "Ahuachapán", "Santa Ana", "Sonsonate", "Chalatenango", "La Libertad", "San Salvador", "Cuscatlán", "La Paz", "Cabañas", "San Vicente", "Usulután", "San Miguel", "Morazán", "La Unión" });
+            cbRegionGraficar.Location = new Point(78, 61);
+            cbRegionGraficar.Name = "cbRegionGraficar";
+            cbRegionGraficar.Size = new Size(121, 23);
+            cbRegionGraficar.TabIndex = 4;
+            // 
+            // btnRegresar
+            // 
+            btnRegresar.Location = new Point(371, 61);
+            btnRegresar.Name = "btnRegresar";
+            btnRegresar.Size = new Size(121, 23);
+            btnRegresar.TabIndex = 5;
+            btnRegresar.Text = "button1";
+            btnRegresar.UseVisualStyleBackColor = true;
+            btnRegresar.Click += btnRegresar_Click;
+            // 
+            // Graficas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRegresar);
+            Controls.Add(cbRegionGraficar);
+            Controls.Add(btnGraficar);
+            Controls.Add(label1);
+            Controls.Add(grafica1);
+            Name = "Graficas";
+            Text = "Form1";
+            Load += Graficas_Load;
+            ((System.ComponentModel.ISupportInitialize)grafica1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafica1;
+        private Label label1;
+        private Button btnGraficar;
+        private ComboBox cbRegionGraficar;
+        private Button btnRegresar;
+    }
+}
---
+++ SistemaDeGestionDeClimas/Ingresar.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text; 
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeGestionDeClimas
+{
+    public partial class Ingresar : Form
+    {
+        public Ingresar()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile("D:\\segundoaño\\modulo2.1\\Programación_DiegoElias#6\\SistemaDeGestionDeClimas\\img\\fondoIngresos.png");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void Ingresar_Load(object sender, EventArgs e)
+        {
+            //Cargar nombres de elementos:
+            label1.Text = "Ingreso de datos climáticos";
+            groupBox1.Text = "Datos:";
+            groupBox2.Text = "Datos cargados:";
+            label2.Text = "Departamento:";
+            label3.Text = "Mes:";
+            label4.Text = "Temperatura (°C):";
+            label5.Text = "%Humedad:";
+            label6.Text = "Precipitación (mm):";
+            btnGuardar.Text = "Guardar dato";
+            btnRegresar.Text = "Regresar";
+
+            //Desde que el formulario se carga se restinge al usuario a que pueda editar el comboBox
+            cbDepartamento.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbMes.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Configuración de encabezados del DataGridView
+            dgvDatos.ColumnCount = 5; // 5 columnas
+
+            dgvDatos.Columns[0].Name = "Departamento";
+            dgvDatos.Columns[1].Name = "Mes";
+            dgvDatos.Columns[2].Name = "Temperatura (°C)";
+            dgvDatos.Columns[3].Name = "Humedad (%)";
+            dgvDatos.Columns[4].Name = "Precipitación (mm)";
+
+            // Opcional: ajustar el ancho de las columnas automáticamente
+            dgvDatos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Declaración de variables:
+            int depaSelec = cbDepartamento.SelectedIndex; //Selecciones del comboBox
+            int mesSelec = cbMes.SelectedIndex;//Selecciones del comboBox
+            double temperatura = double.Parse(txtTemperatura.Text);
+            double humedad = double.Parse(txtHumedad.Text);
+            double precipitacion = double.Parse(txtPrecipitacion.Text);
+
+            string depaNombre = Convert.ToString(cbDepartamento.SelectedItem);
+            string mesNombre = Convert.ToString(cbMes.SelectedItem);
+
+
+            //
+            //Condiciones para que se ejecute el código
+            //
+
+            //Validación de que las selecciones de los comboBox no sean nulos
+            if (depaSelec < 0 || mesSelec < 0)
+            {
+                MessageBox.Show("Seleccione una región y un mes.");
+                return;
+            }
+
+            //Validación de que la temperatura ingresada no sea nula
+            if ((temperatura <= 0))
+            {
+                MessageBox.Show("Valor de: temperatura inválido.");
+                return;
+            }
+
+            //Validación de que la humedad ingresada no sea nula
+            if (humedad <= 0)
+            {
+                MessageBox.Show("Valor de: humedad inválido.");
+                return;
+            }
+
+            // Validación de que la precipitación no sea nula
+            if (precipitacion <= 0)
+            {
+                MessageBox.Show("Valor de: precipitación inválido.");
+                return;
+            }
+
+            //
+            //Fin de las condiciones
+            //
+
+            //
+            // Verifiación con las selecciones de los listBox y parámetros
+            //
+
+            //Parámetro 0 en array de datosClimaticos es igual a la temperatura
+            if (Main.datosClimaticos[depaSelec, mesSelec, 0] != 0 || Main.datosClimaticos[depaSelec, mesSelec, 0] != 0)
+            {
+                MessageBox.Show("Posición ocupada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Parámetro 1 en array de datosClimaticos es igual a la humedad
+            if (Main.datosClimaticos[depaSelec, mesSelec, 1] != 0 || Main.datosClimaticos[depaSelec, mesSelec, 1] != 0)
+            {
+                MessageBox.Show("Posición ocupada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Parámetro 2 en array de datosClimaticos es igual a la precipitación
+            if (Main.datosClimaticos[depaSelec, mesSelec, 1] != 0 || Main.datosClimaticos[depaSelec, mesSelec, 1] != 0)
+            {
+                MessageBox.Show("Posición ocupada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            //
+            // Verifiación con las selecciones de los listBox y parámetros
+            //
+
+            //Guardo los datos en el array de tres dimensiones
+            //Estos datos indican que la selección de la región y mes se guarda en la casilla de xy, y
+            // los espacios de los niveles (0,1,2) se les guarda los datos de temperatura, humedad y precipitación.
+            Main.datosClimaticos[depaSelec, mesSelec, 0] = temperatura;
+            Main.datosClimaticos[depaSelec, mesSelec, 1] = humedad;
+            Main.datosClimaticos[depaSelec, mesSelec, 2] = precipitacion;
+
+            //Agrego los datos al listBox(listBox1) y al DataGridView(dgvDatos)
+            //Posteriormente los muestro en el lb o dgv
+            lbDatos.Items.Add($"{depaNombre} - {mesNombre}: Temp {temperatura}°C, Hum {humedad}%, Prec {precipitacion}mm");
+            dgvDatos.Rows.Add(depaNombre, mesNombre, temperatura, humedad, precipitacion);
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
