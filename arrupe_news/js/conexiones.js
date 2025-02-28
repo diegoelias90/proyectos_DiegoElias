489f0a2e1b61f89d607cb3c29654ee309e0b3163
Diego Elias <eliaslopezdiegojosue@gmail.com>
2025-02-27 21:10:53 -0600

    Proyecto de Arrupe news

---
+++ arrupe_news/js/conexiones.js
@@ -0,0 +1,31 @@
+function Torneo(){
+    window.location.href = "pages/torneo.html";
+}
+
+function Promo(){
+    window.location.href = "pages/promo.html";
+}
+
+function Estudio(){
+    window.location.href = "pages/estudio.html";
+}
+
+function Articulos(){
+    window.location.href = "pages/articulos.html";
+}
+
+function Diversion(){
+    window.location.href = "pages/diversion.html";
+}
+
+function Entrevista(){
+    window.location.href = "pages/entrevista.html";
+}
+
+function Instagram(){
+    window.open("https://www.instagram.com/colegiopadrearrupeoficial/", "_blank");
+}
+
+function GoBack(){
+    window.location.href = "../index.html";
+}
